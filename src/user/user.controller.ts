98364ef466e5ce60d62a312999c9140f3ee258c7
@ali_abdebaqy <ali.abdelbaqy@grand-community.com>
2023-04-27 22:18:56 +0200

    opt

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,16 @@
 // eslint-disable-next-line prettier/prettier
-import { Controller,Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Param } from '@nestjs/common';
+import { Request } from 'express';
 @Controller('users')
 // eslint-disable-next-line prettier/prettier
 export class UserController {
   @Get('a')
   getAll(): string[] {
     return ['ali', 'admin'];
+  }
+
+  @Post(':username')
+  create(@Param() username: string): string {
+    return username;
   }
 }
